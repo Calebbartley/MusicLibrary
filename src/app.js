87b310629078f741eb2687b91563540fd9895abb
calebbartley <calebbartley18@gmail.com>
2021-07-27 22:46:19 -0400

    state library created from api

--- src/app.js
+++ src/app.js
@@ -1,44 +1,49 @@
 import React, { Component } from 'react';
-import TitleBar from './Components/Title/TitleBar';
-import musicList from './Components/MusicTable/musicTable';
+// import TitleBar from './Components/Title/TitleBar';
+import Song from './Components/song/Song';
+// import musicList from './Components/MusicTable/musicTable';
 import axios from 'axios';
 
 import './app.css';
 
 
-const api = axios.create({
-    baseURL:'http://www.devcodecampmusiclibrary.com/'
-})
+// const api = axios.create({
+//     baseURL:'http://www.devcodecampmusiclibrary.com/'
+// })
 
 
 
 
 class App extends Component{
 
-    state= {
-        library:[]
-    }
+
     constructor(props){
         super(props);
-        api.get('/').then(res =>{
-            console.log(res.data)
-            this.setState({library: res.data})
-        })
+
+        this.state={
+            library:[]
+        }
     }
 
-    createLib = async ()=>{
-        let res = await api.post('/',{title:'',id: '',author:''})
-        console.log(res)
-    }
+    // createLib = async ()=>{
+    //     let res = await api.post('/',{title:'',id:'',author:''})
+    //     console.log(res)
+    // }
 
     componentDidMount(){
         this.makeGetRequest();
     }
 
+    
+
     async makeGetRequest(){
         try{
-            let response = await axios.get(`http://www.devcodecampmusiclibrary.com/`);
+            let response = await axios.get('http://www.devcodecampmusiclibrary.com/api/music');
             console.log(response.data)
+            this.setState({
+                library:response.data
+            })
+
         }
         catch (ex) {
             console.log(`error in api call`);
@@ -47,14 +52,19 @@ class App extends Component{
 
     render(){
         return(
-            <div className="app">
-            {this.state.library.map(lib => <h1 key={lib.id}>{lib.title}</h1>)}
-                <musicList musicList={this.state.listOfSongs}/>
-                <TitleBar/>
-                <button onClick={this.makeGetRequest}>Remake</button>
-                <h1>Hello World</h1>
+            <div>
+                <Song state={this.state}/>
             </div>
         )
+        // return(
+        //     <div className="app">
+        //     <button onClick={this.createLib}>library</button>
+        //     {this.state.library.map(lib => <h1 key={lib.id}>{lib.title}</h1>)}
+        //         <musicList musicList={this.state.listOfSongs}/>
+        //         <TitleBar/>
+        //         <h1>Hello World</h1>
+        //     </div>
+        // )
     }
 }
 
