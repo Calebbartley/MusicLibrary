52854faaf8fbc4b23735e5f2baa526f1e7f0f45f
markram0s <81868550+markram0s@users.noreply.github.com>
2021-08-04 21:53:52 -0400

    updated styling on the main song display list

--- src/Components/song/Song.js
+++ src/Components/song/Song.js
@@ -6,13 +6,11 @@ const Song = (props) => {
         <div className="h1">
             {props.library.map((song, index) => (
             <span>
-
                 <h1>Title: {song.title}</h1>
                 <h1>Artist: {song.artist} </h1>
-                <h3>Album: {song.album}</h3>
-                <h3>Genre: {song.genre} </h3>
-                <h3>Release Date: {song.releaseDate}</h3>
-                
+                <h1>Album: {song.album}</h1>
+                <h1>Genre: {song.genre} </h1>
+                <h1>Release Date: {song.releaseDate}</h1>
                 <br></br>
             </span>
             ))}
